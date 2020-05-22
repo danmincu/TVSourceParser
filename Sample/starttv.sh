c938cb9b4982020d671ed71988fa3026df079f9b
Dan Mincu <danmincu@gmail.com>
2020-05-22 11:29:18 -0400

    fix for screen starting the acehttp.py

--- Sample/starttv.sh
+++ Sample/starttv.sh
@@ -1,6 +1,6 @@
 killall vlc
 screen -S acestream -d -m acestreamengine --client-console
-screen -S aceproxy -d -m python aceproxy/acehttp.py
+screen -S aceproxy -d -m python /home/user/aceproxy/acehttp.py
 rm /home/user/Desktop/playlist.xspf
 rm playlist.xspf
 wget https://raw.githubusercontent.com/danmincu/TVSourceParser/master/Sample/playlist.xspf
